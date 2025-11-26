f6f906aec4e8474b7d89af4d8943d366fc0c6d7a
gowthamtraining <gouthamvenkatesh21@gmail.com>
2025-11-27 00:50:31 +0530

    build fixes

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,22 +1,13 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_BASE_URL,
+    baseURL: import.meta.env.VITE_API_BASE_URL || '/api',
     headers: {
         'Content-Type': 'application/json'
     }
 });
 
-// Interceptor to add token to requests
-api.interceptors.request.use(
-    config => {
-        const token = localStorage.getItem('token');
-        if (token) {
-            config.headers['x-auth-token'] = token;
-        }
-        return config;
-    },
-    error => Promise.reject(error)
-);
+console.log('API Base URL:', api.defaults.baseURL);
+
 
 export default api;
